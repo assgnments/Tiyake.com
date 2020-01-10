52af099e57592af03fcdebbb500bb0c006425e94
yaredgdb123 <46164446+yaredgdb123@users.noreply.github.com>
2020-01-10 15:32:39 +0300

    Add files via upload

---
+++ answer/service/answer_service.go
@@ -0,0 +1,60 @@
+package service
+
+import (
+	"../../entity"
+)
+
+// AnswerService implements menu.AnswerService interface
+type AnswerService struct {
+	AnswerRepo Answer.AnswerRepository
+}
+
+// NewAnswerService returns a new AnswerService object
+func NewAnswerService(commRepo Answer.AnswerRepository) Answer.AnswerService {
+	return &AnswerService{AnswerRepo: commRepo}
+}
+
+// Answers returns all stored Answers
+func (cs *AnswerService) Answers() ([]entity.Answer, []error) {
+	cmnts, errs := cs.AnswerRepo.Answers()
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return cmnts, errs
+}
+
+// Answer retrieves stored Answer by its id
+func (cs *AnswerService) Answer(id uint) (*entity.Answer, []error) {
+	cmnt, errs := cs.AnswerRepo.Answer(id)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return cmnt, errs
+}
+
+// UpdateAnswer updates a given Answer
+func (cs *AnswerService) UpdateAnswer(Answer *entity.Answer) (*entity.Answer, []error) {
+	cmnt, errs := cs.AnswerRepo.UpdateAnswer(Answer)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return cmnt, errs
+}
+
+// DeleteAnswer deletes a given Answer
+func (cs *AnswerService) DeleteAnswer(id uint) (*entity.Answer, []error) {
+	cmnt, errs := cs.AnswerRepo.DeleteAnswer(id)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return cmnt, errs
+}
+
+// StoreAnswer stores a given Answer
+func (cs *AnswerService) StoreAnswer(Answer *entity.Answer) (*entity.Answer, []error) {
+	cmnt, errs := cs.AnswerRepo.StoreAnswer(Answer)
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return cmnt, errs
+}
